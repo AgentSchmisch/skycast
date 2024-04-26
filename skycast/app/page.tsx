c80d691d979a418295f322182d2154853e76cdd5
cc231067 <cc231067@fhstp.ac.at>
2024-04-26 18:39:23 +0200

    ✨ created CityBox component, Heroimage, and other basic components

--- skycast/app/page.tsx
+++ skycast/app/page.tsx
@@ -1,20 +1,30 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import CityBox from '@/app/ui/cityBox';
+import React from 'react';
+import SearchBar from '@/app/ui/components/SearchBar';
+import NavBar from './ui/components/NavBar';
+import HeroImage from './ui/components/HeroImage';
+import CityBox from './ui/components/CityBox';
 
 export default function Page() {
-  const cities = ["Vienna", "Linz", "Graz", "St. Pölten", "Salzburg", "Innsbruck", "Klagenfurt"];
+  const cities = ["Vienna", "Linz", "Graz", "St. Pölten", "Salzburg", "Innsbruck"];
 
   return (
-    <main className="flex flex-col p-6">
-      <h1>Austria</h1>
-      <div className='flex flex-row'>
-      {cities.map((city, index) => (
-        <div key={index} className='pr-6'>
-        <CityBox city={city} />
+    <main className="flex flex-col">
+      <HeroImage />
+      <NavBar activePage="index" />
+      <div className='flex flex-col items-center m-4'>
+        <div className='flex flex-col w-3/4'>
+        
+        <div className="flex flex-row justify-between items-center">
+        <h1 className="text-xl font-bold text-[#284B63]">Our Weather Prediction</h1>
+        <SearchBar/>
         </div>
-      ))}
+
+        <h2 className='font-bold py-2 text-lg text-[#284B63]'>Austria</h2>
+          <CityBox cities={cities} />
+        </div>
       </div>
     </main>
   );
--- skycast/app/ui/components/HeroImage.tsx
+++ skycast/app/ui/components/HeroImage.tsx
@@ -1,5 +1,5 @@
 export default function HeroImage() {
     return (
-            <img src="/img/vienna.jpg" alt="Hero image of the city" className="inset-0 object-cover h-96" />
+            <img src="/img/vienna.jpg" alt="Hero image of the city" className="inset-0 object-cover h-[40rem]" />
     );
 }

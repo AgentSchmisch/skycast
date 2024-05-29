6644fd3d2f2c58331d461313409febe7d1e930bd
cc231067 <cc231067@fhstp.ac.at>
2024-05-29 14:11:37 +0200

    ✨ added cities overview page

--- skycast/app/cities/page.tsx
+++ skycast/app/cities/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-import React, {useRef} from 'react';
+import { useRef, useState } from 'react';
 import LandingBanner from "@/app/ui/components/LandingBanner";
-import NavBar from "@/app/ui/components/NavBar";
+import CitiesSearchbar from "@/app/ui/components/cities/CitiesSearchbar";
+import CitiesBanner from "@/app/ui/components/cities/CitiesBanner";
+import NavBar from '@/app/ui/components/NavBar';
 
 export default function Page() {
-    const mainRef = useRef<HTMLElement|null>(null)
-    return (
-        <main className="flex flex-col" ref={mainRef}>
-            <LandingBanner />
-            <NavBar target={null} />
-      <h1>This is the all cities page</h1>
+  const [result, setResult] = useState("");
+
+  const mainRef = useRef<HTMLElement | null>(null)
+  return (
+    <main className="flex flex-col" ref={mainRef}>
+      <NavBar target={null}/>
+      <CitiesBanner />
+      <CitiesSearchbar setResult={setResult} />
     </main>
   );
 }
